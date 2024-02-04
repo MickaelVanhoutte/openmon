b44a9f76c179fe883a32e11d569e16fa35556bd9
Vanhoutte Mickael <mickael.vanhoutte@leroymerlin.fr>
2024-02-04 22:08:05 +0100

    fix: mobile

--- src/lib/screens/Battle.svelte
+++ src/lib/screens/Battle.svelte
@@ -1,4 +1,4 @@
-<div bind:this={gifsWrapper}>
+<div bind:this={gifsWrapper} class="wrapper">
 
 </div>
 
@@ -34,9 +34,9 @@
 
     if (battleState) {
         battleState.onClose = (win: boolean) => {
-            if(!win){
+            if (!win) {
                 // tp back to the start
-                this.context.map.playerMovedOffset = new Position(0,0);
+                this.context.map.playerMovedOffset = new Position(0, 0);
                 this.context.pla
             }
             setTimeout(() => {
@@ -60,7 +60,7 @@
         bgDrawn: false
     }
 
-function battle() {
+    function battle() {
 
         setInterval(() => {
             if (!battleLoopContext.bgDrawn) {
@@ -88,14 +88,12 @@ function battle() {
                 if (!battleLoopContext.opponentdrawn) {
 
                     let opponent = document.createElement('img') as HTMLImageElement;
+                    opponent.classList.add('opponent-sprite');
                     opponent.src = battleState?.opponentCurrentMonster.sprites?.male?.front.frame1 || 'src/assets/monsters/bw/0.png';
                     opponent.onload = () => {
-                        opponent.style.position = 'absolute';
-                        opponent.style.bottom = '50%';
-                        opponent.style.right = '10%';
-                        opponent.style.zIndex = '8';
-                        opponent.style.width = opponent.naturalWidth * 1.5 + 'px';
-                        opponent.style.height = opponent.naturalHeight * 1.5 + 'px';
+
+                        opponent.style.setProperty('--width', opponent.naturalWidth + 'px');
+                        opponent.style.setProperty('--height', opponent.naturalHeight + 'px');
                         gifsWrapper.appendChild(opponent);
                         battleLoopContext.opponentdrawn = true;
                     }
@@ -103,13 +101,12 @@ function battle() {
                 if (!battleLoopContext.allydrawn) {
                     let ally = document.createElement('img') as HTMLImageElement;
                     ally.src = battleState?.playerCurrentMonster.sprites?.male?.back.frame1 || 'src/assets/monsters/bw/0.png';
+                    ally.classList.add('ally-sprite');
                     ally.onload = () => {
-                        ally.style.position = 'absolute';
-                        ally.style.bottom = '25%';
-                        ally.style.left = '0';
-                        ally.style.zIndex = '8';
-                        ally.style.width = ally.naturalWidth + 'px';
-                        ally.style.height = ally.naturalHeight + 'px';
+                        console.log(ally.naturalWidth, ally.width);
+
+                        ally.style.setProperty('--width', ally.naturalWidth + 'px');
+                        ally.style.setProperty('--height', ally.naturalHeight + 'px');
                         gifsWrapper.appendChild(ally);
                         battleLoopContext.allydrawn = true;
                     }
@@ -117,104 +114,12 @@ function battle() {
 
             }
         }, 200);
-}
-    //deprecate
-    function battleLoop() {
-        battleLoopContext.id = window.requestAnimationFrame(battleLoop);
-        let now = Date.now();
-        let elapsed = now - battleLoopContext.then;
-
-        if (!battleState) {
-            return;
-        }
-
-        if (elapsed > battleLoopContext.fpsInterval) {
-
-
-            battleLoopContext.then = now - (elapsed % battleLoopContext.fpsInterval);
-            battleLoopContext.frameElapsed++;
-
-            ctx.fillStyle = 'black';
-            ctx.fillRect(0, 0, canvas.width, canvas.height);
-
-            //drawer.draw(ctx, battleState.pokemonsAppearing, 'grass');
-
-            if(!battleLoopContext.bgDrawn) {
-                let bg = document.createElement('img') as HTMLImageElement;
-                bg.src = 'src/assets/battle/battle-grass.png';
-                bg.onload = () => {
-                    bg.style.position = 'absolute';
-                    bg.style.bottom = '25%';
-                    bg.style.left = '0';
-                    bg.style.zIndex = '0';
-                    bg.style.width = window.innerWidth + 'px';
-                    bg.style.height = window.innerHeight * 0.75 + 'px';
-                    gifsWrapper.appendChild(bg);
-                    battleLoopContext.bgDrawn = true;
-                }
-            }
-
-
-            if (!battleState.pokemonsAppearing) {
-                //opponentDrawer.draw(ctx, battleState.opponentCurrentMonster, 'front', true, 0, true);
-                //allyDrawer.draw(ctx, battleState.playerCurrentMonster, 'back', true, 0);
-
-                // animated gifs, handle them outside the canvas
-                if (!battleLoopContext.opponentdrawn) {
-
-                    let opponent = document.createElement('img') as HTMLImageElement;
-                    opponent.src = battleState.opponentCurrentMonster.sprites?.male?.front.frame1 || 'src/assets/monsters/bw/0.png';
-                    opponent.onload = () => {
-                        opponent.style.position = 'absolute';
-                        opponent.style.bottom = '50%';
-                        opponent.style.right = '15%';
-                        opponent.style.zIndex = '8';
-                        opponent.style.width = opponent.naturalWidth * 1.5 + 'px';
-                        opponent.style.height = opponent.naturalHeight * 1.5 + 'px';
-                        gifsWrapper.appendChild(opponent);
-                        battleLoopContext.opponentdrawn = true;
-                    }
-                }
-                if (!battleLoopContext.allydrawn) {
-                    let ally = document.createElement('img') as HTMLImageElement;
-                    ally.src = battleState.playerCurrentMonster.sprites?.male?.back.frame1 || 'src/assets/monsters/bw/0.png';
-                    ally.onload = () => {
-                        ally.style.position = 'absolute';
-                        ally.style.bottom = '25%';
-                        ally.style.left = '0';
-                        ally.style.zIndex = '8';
-                        ally.style.width = ally.naturalWidth  + 'px';
-                        ally.style.height = ally.naturalHeight  + 'px';
-                        gifsWrapper.appendChild(ally);
-                        battleLoopContext.allydrawn = true;
-                    }
-                }
-
-            }
-
-            if (battleLoopContext.debug) {
-                ctx.fillStyle = 'black';
-                ctx.fillRect(canvas.width / 3 + 100, 40, 160, 60);
-
-                ctx.fillStyle = 'white';
-                ctx.fillText('attack: ' + battleState.opponentCurrentMonster.currentStats.attack, canvas.width / 3 + 100 + 10, 60);
-                ctx.fillText('speed: ' + battleState.opponentCurrentMonster.currentStats.speed, canvas.width / 3 + 100 + 10, 80);
-
-                ctx.fillStyle = 'black';
-                ctx.fillRect(40, canvas.height / 4 * 3 - 120, 160, 60);
-
-                ctx.fillStyle = 'white';
-                ctx.fillText('attack: ' + battleState.playerCurrentMonster.currentStats.attack, 50, canvas.height / 4 * 3 - 100);
-                ctx.fillText('speed: ' + battleState.playerCurrentMonster.currentStats.speed, 50, canvas.height / 4 * 3 - 80);
-
-            }
-
-        }
     }
 
+    //deprecate
+
     onMount(() => {
-        //battleLoop();
-battle();
+        battle();
         window.addEventListener('keydown', (e) => {
             if (e.key === 'x') {
                 battleLoopContext.debug = !battleLoopContext.debug;
@@ -224,5 +129,21 @@ battle();
 </script>
 
 <style lang="scss">
+  .wrapper {
+    font-size: 23px;
+  }
 
+  .wrapper :global(.ally-sprite) {
+    position: absolute;
+    bottom: 25%;
+    left: calc(25% - var(--width) / 2);
+    z-index: 8;
+  }
+
+  .wrapper :global(.opponent-sprite) {
+    position: absolute;
+    bottom: calc(64% - var(--height) / 2);
+    right: calc(25% - var(--width) / 2);
+    z-index: 8;
+  }
 </style>
