3ddc21c67f69d7a053a35536d21531c3976b4886
Vanhoutte Mickael <mickael.vanhoutte@leroymerlin.fr>
2024-02-15 19:48:35 +0100

    chore: publish

--- docs/src/assets/tiled/tileset.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.10.2" name="tileset" tilewidth="16" tileheight="16" spacing="1" margin="1" tilecount="1316" columns="28">
- <image source="../tileset/firered/tileset.png" width="477" height="800"/>
-</tileset>
