ef65d171e0c96ba6b338524a5a933cd7e0b5a8ec
Vanhoutte Mickael <mickael.vanhoutte@leroymerlin.fr>
2024-02-17 09:40:40 +0100

    feat: wip bag

--- src/lib/js/const.ts
+++ src/lib/js/const.ts
@@ -1,7 +1,6 @@
 import {Pokedex} from "./pokemons/pokedex";
 import pokedexJson from "../../assets/data/final/pokedexBW-animated2.json";
 import charactersJson from "../../assets/characts/characts.json";
-import itemsJson from "../../assets/data/final/usable-items.json";
 import {SpritesHolder, WoldSpriteDrawer} from "./sprites/drawers";
 import {writable} from "svelte/store";
 import {BattleContext} from "./battle/battle";
@@ -10,9 +9,9 @@ import {container} from "tsyringe";
 import {MoveEffectApplier} from "./pokemons/move-effects";
 import {start} from "./mapping/maps/start";
 import {startVillage} from "./mapping/maps/start-village";
-import type {AItem} from "./items/items";
-import {HealingItem, Pokeball, ReviveItem} from "./items/items";
+import {ItemsReferences} from "./items/items";
 
+export const ITEMS = new ItemsReferences();
 export let POKEDEX = new Pokedex(pokedexJson);
 
 // Sprites
@@ -28,22 +27,3 @@ export const MAPS = {
     0: start,
     1: startVillage,
 }
-
-export let ITEMS = new Map<number, AItem>();
-itemsJson.forEach((item: any) => {
-    switch (item.category_id) {
-        case 34:
-            ITEMS.set(item.id, new Pokeball(item.id, item.category_id, item.name, item.description, item.power));
-            break;
-        case 27:
-            ITEMS.set(item.id, new HealingItem(item.id, item.category_id, item.name, item.description, item.power));
-            break;
-        case 29:
-            ITEMS.set(item.id, new ReviveItem(item.id, item.category_id, item.name
-                , item.description, item.power));
-            break;
-        default:
-            break;
-
-    }
-});
