99383df5a9e97c22ff22f3d8efe3300b9eba433d
Vanhoutte Mickael <mickael.vanhoutte@leroymerlin.fr>
2024-02-21 18:29:25 +0100

    feat: menu improvment

--- src/lib/ui/Menu.svelte
+++ src/lib/ui/Menu.svelte
@@ -5,81 +5,94 @@
 
 
             <li class="hex">
-                <div class="hexIn" class:selected={selected === 0}>
-                    <a class="hexLink" on:click={() => openList()}>
-                        <img src="src/assets/menus/pokeball.png" alt="pokemons">
-                        <h1>Pokémons</h1>
-                        <div class='img'></div>
-                    </a>
+                <div class="hexIn" class:selected={selected === 1}>
+                    <button class="hexLink" on:click={() => openBoxes()} tabindex="1">
+                        <img src="src/assets/menus/boxes.png" alt="pc boxes">
+                        <span class="title">Boxes</span>
+                        <span class='img'></span>
+                    </button>
                 </div>
             </li>
 
-            <li class="hex">
-                <div class="hexIn" class:selected={selected === 1}>
-                    <a class="hexLink" on:click={() => openBoxes()}>
-                        <img src="src/assets/menus/boxes.png" alt="pc boxes">
-                        <h1>Boxes</h1>
-                        <div class='img'></div>
-                    </a>
-                </div>
-            </li>
 
             <li class="hex">
                 <div class="hexIn" class:selected={selected === 2}>
-                    <a class="hexLink" on:click={() => pokedex()}>
-                        <img src="src/assets/menus/pokedex.png" alt="pc boxes">
-                        <h1>Pokedex</h1>
-                        <div class='img'></div>
-                    </a>
+                    <button class="hexLink" on:click={() => openBag()} tabindex="1">
+                        <img src="src/assets/menus/bag.png" alt="bag">
+                        <span class="title">Bag</span>
+                        <span class='img'></span>
+                    </button>
                 </div>
+            </li>
+
+
+            <li class="hex">
+                <div class="hexIn" class:selected={selected === 0}>
+                    <button class="hexLink" on:click={() => openList()} tabindex="1">
+                        <img src="src/assets/menus/pokeball.png" alt="pokemons">
+                        <span class="title">Pokémons</span>
+                        <span class='img'></span>
+                    </button>
+                </div>
+            </li>
+
+
+            <li class="hex">
+                <!-- EMPTY MIDDLE -->
             </li>
 
 
             <li class="hex">
                 <div class="hexIn" class:selected={selected === 3}>
-                    <a class="hexLink" on:click={() => openBag()}>
-                        <img src="src/assets/menus/bag.png" alt="bag">
-                        <h1>Bag</h1>
-                        <div class='img'></div>
-                    </a>
+                    <button class="hexLink" on:click={() => pokedex()} tabindex="1">
+                        <img src="src/assets/menus/pokedex.png" alt="pc boxes">
+                        <span class="title">Pokedex</span>
+                        <span class='img'></span>
+                    </button>
                 </div>
             </li>
-            <li class="hex">
-                <div class="hexIn" class:selected={selected === 4}>
-                    <a class="hexLink" on:click={() => trainer()}>
-                        <img src="src/assets/menus/trainer.png" alt="trainer">
-                        <h1>Trainer</h1>
-                        <div class='img'></div>
-                    </a>
-                </div>
-            </li>
+
             <li class="hex">
                 <div class="hexIn" class:selected={selected === 5}>
-                    <a class="hexLink" on:click={()=> saveCurrent()}>
+                    <button class="hexLink" on:click={()=> saveCurrent()} tabindex="1">
                         <img src="src/assets/menus/save.png" alt="save">
-                        <h1>Save</h1>
-                        <div class='img'></div>
-                    </a>
+                        <span class="title">Save</span>
+                        <span class='img'></span>
+                    </button>
                 </div>
             </li>
+
+
+            <li class="hex">
+                <div class="hexIn" class:selected={selected === 4}>
+                    <button class="hexLink" on:click={() => trainer()} tabindex="1">
+                        <img src="src/assets/menus/trainer.png" alt="trainer">
+                        <span class="title">Trainer</span>
+                        <span class='img'></span>
+                    </button>
+                </div>
+            </li>
+
+
+            <!-- <li class="hex">
+                 <div class="hexIn" class:selected={selected === 6}>
+                     <button class="hexLink" on:click={()=> settings()} tabindex="1">
+                         <img src="src/assets/menus/settings.png" alt="settings">
+                         <span class="title">Settings</span>
+                         <span class='img'></span>
+                     </button>
+                 </div>
+             </li>-->
 
             <li class="hex">
                 <div class="hexIn" class:selected={selected === 6}>
-                    <a class="hexLink" on:click={()=> settings()}>
-                        <img src="src/assets/menus/settings.png" alt="settings">
-                        <h1>Settings</h1>
-                        <div class='img'></div>
-                    </a>
-                </div>
-            </li>
-
-            <li class="hex">
-                <div class="hexIn" class:selected={selected === 7}>
-                    <a class="hexLink" on:click={() => close()}>
-                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor"><path d="M19.0001 10.0001L19.0003 19L17.0003 19L17.0002 12.0001L9.41409 12V17.4142L2.99988 11L9.41409 4.58578L9.41409 10L19.0001 10.0001Z"></path></svg>
-                        <h1>Back</h1>
-                        <div class='img'></div>
-                    </a>
+                    <button class="hexLink" on:click={() => close()} tabindex="1">
+                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor">
+                            <path d="M19.0001 10.0001L19.0003 19L17.0003 19L17.0002 12.0001L9.41409 12V17.4142L2.99988 11L9.41409 4.58578L9.41409 10L19.0001 10.0001Z"></path>
+                        </svg>
+                        <span class="title">Back</span>
+                        <span class='img'></span>
+                    </button>
                 </div>
             </li>
         </ul>
@@ -161,26 +174,26 @@
 
     const listener = (e: KeyboardEvent) => {
         if (menuOpened) {
-            if (e.key === "ArrowUp") {
-                selected = selected === 0 ? 7 : selected - 1;
-            } else if (e.key === "ArrowDown") {
-                selected = selected === 7 ? 0 : selected + 1;
+            if (e.key === "ArrowDown") {
+                selected = selected === 0 ? 6 : selected - 1;
+            } else if (e.key === "ArrowUp") {
+                selected = selected === 6 ? 0 : selected + 1;
             } else if (e.key === "Enter") {
                 if (selected === 0) {
                     openList();
-                } else if(selected === 1) {
+                } else if (selected === 1) {
                     openBoxes()
-                } else if(selected === 2){
-                    pokedex();
-                } else if (selected === 3) {
+                } else if (selected === 2) {
                     openBag();
+                } else if (selected === 3) {
+                    pokedex();
                 } else if (selected === 4) {
                     trainer(); // merge with settings ?
                 } else if (selected === 5) {
                     saveCurrent();
-                } else if (selected === 6) {
+                } /*else if (selected === 6) {
                     settings();
-                } else if (selected === 7) {
+                } */ else if (selected === 6) {
                     close();
                 }
             }
@@ -202,7 +215,8 @@
     left: 0;
     width: 100%;
     height: 100%;
-    background-color: rgba(44, 56, 69, 0.85);
+    background: rgba(44, 56, 69, .6);
+    background: radial-gradient(circle, rgba(0, 0, 0, 0.1) 0%, rgba(0, 0, 0, 0.6) 36%, rgba(0, 0, 0, .75) 50%, rgba(0, 0, 0, 1) 100%);
     z-index: -3;
     opacity: 0;
     transition: all .2s ease-in-out;
@@ -213,11 +227,11 @@
     }
 
     .grid {
-      width: 80dvh;
+      width: 66dvh;
       position: absolute;
       top: 50%;
       left: 50%;
-      transform: translate(-57%, -50%);
+      transform: translate(-60%, -42%);
 
 
       #hexGrid {
@@ -234,10 +248,9 @@
         position: relative;
         visibility: hidden;
         outline: 1px solid transparent; /* fix for jagged edges in FF on hover transition */
-        transition: all 0.5s;
+        transition: all 0.2s;
         backface-visibility: hidden;
         will-change: transform;
-        transition: all 0.5s;
       }
 
       .hex::after {
@@ -257,7 +270,14 @@
         -webkit-transform: rotate3d(0, 0, 1, -60deg) skewY(30deg);
         -ms-transform: rotate3d(0, 0, 1, -60deg) skewY(30deg);
         transform: rotate3d(0, 0, 1, -60deg) skewY(30deg);
-        transition: all 0.5s;
+        transition: all 0.2s;
+
+        button {
+          padding: 0;
+          border: none;
+          outline: none;
+          background: none;
+        }
       }
 
 
@@ -285,7 +305,8 @@
         width: auto;
         top: 35%;
         left: 50%;
-        transform: translate(-50%, -50%);
+        transform-origin: left;
+        transform: rotate(0deg) translate(-50%, -50%) ;
         z-index: 2;
       }
 
@@ -293,19 +314,22 @@
         height: 33%;
       }
 
-      .hex h1, .hex p {
+      .hex span.title, .hex p {
         width: 100%;
         box-sizing: border-box;
-        font-weight: 300;
-        font-size: 26px;
+        font-weight: 500;
+        font-size: 20px;
         font-family: pokemon, serif;
-        text-shadow: 1px 1px 6px black;
-        //margin-top: 70%;
         opacity: 1;
         z-index: 1;
-        color: rgba(255, 255, 255, .65);
+        // color: rgba(0, 0, 0, .65);
+        //text-shadow: 1px 1px 6px white;
+        color: white;
+        text-shadow: none;
         position: absolute;
-        bottom: 6%;
+        bottom: 22%;
+        left: 0;
+        text-transform: uppercase;
       }
 
 
@@ -315,11 +339,48 @@
         right: 0;
         bottom: 0;
         left: 0;
-        background: #359cc4;
+        //background: rgba(255, 255, 255, .6);
+        background: rgba(53, 156, 196, .8);
         background-size: cover;
         overflow: hidden;
         -webkit-clip-path: polygon(50% 0%, 100% 25%, 100% 75%, 50% 100%, 0% 75%, 0% 25%);
         clip-path: polygon(50% 0%, 100% 25%, 100% 75%, 50% 100%, 0% 75%, 0% 25%);
+      }
+
+      .hexIn.selected {
+        span.title {
+          //color: white;
+          //text-shadow: 1px 1px 6px black;
+          color: rgba(0, 0, 0, .85);
+        }
+
+        img, svg {
+          position: absolute;
+          animation: shake 0.5s ease-in-out infinite;
+        }
+
+        .img {
+          background: rgba(255, 255, 255, .6);
+          //background: rgba(53, 156, 196, .8);
+        }
+      }
+
+      @keyframes shake {
+        0% {
+          transform: rotate(0deg) translate(-50%, -50%);
+        }
+        25% {
+          transform: rotate(5deg) translate(-50%, -50%);
+        }
+        50% {
+          transform: rotate(0) translate(-50%, -50%);
+        }
+        75% {
+          transform: rotate(-5deg) translate(-50%, -50%);
+        }
+        100% {
+          transform: rotate(0deg) translate(-50%, -50%);
+        }
       }
 
       .img:before, .img:after {
@@ -330,21 +391,13 @@
         left: 0;
         content: '';
         opacity: 0;
-        transition: opacity 0.5s;
+        transition: opacity 0.2s;
       }
 
-      .img:before {
-        background: rgba(22, 103, 137, 0.3)
-      }
-
-      .img:after {
-        background: linear-gradient(to top, transparent, rgba(0, 0, 0, 0.5), transparent);
-      }
-
-      .hexLink:hover h1, .hexLink:focus h1,
-      .hexLink:hover h1, .hexLink:focus p {
+      .hexLink:hover span.title, .hexLink:focus span.title,
+      .hexLink:hover p, .hexLink:focus p {
         opacity: 1;
-        transition: 0.8s;
+        transition: 0.2s;
       }
 
 
@@ -356,15 +409,15 @@
       .hexIn:hover .hexLink {
         opacity: 1;
 
-        .hexLink h1, .hexLink p {
+        .hexLink span.title, .hexLink p {
           opacity: 1;
-          transition: 0.8s;
+          transition: 0.2s;
         }
       }
 
 
       #hexGrid {
-        padding-bottom: calc(7.4%);
+        padding-bottom: 8%;
         font-size: 14px;
       }
 
