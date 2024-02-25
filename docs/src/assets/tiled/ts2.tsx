e559a9a73e4c5c42e1ad2f5427fb6936aa5b577f
Vanhoutte Mickael <mickael.vanhoutte@leroymerlin.fr>
2024-02-25 09:21:44 +0100

    chore: publish

--- docs/src/assets/tiled/ts2.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.10.2" name="ts2" tilewidth="32" tileheight="32" tilecount="5208" columns="8">
- <image source="../tileset/tileset2.png" width="256" height="20832"/>
-</tileset>
