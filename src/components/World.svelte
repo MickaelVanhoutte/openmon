aef7abaf4ac9697302a0b1e3eb24c9cd17adf0cf
Vanhoutte Mickael <mickael.vanhoutte@leroymerlin.fr>
2024-02-29 23:09:31 +0100

    feat: run toggle

--- src/components/World.svelte
+++ src/components/World.svelte
@@ -23,10 +23,26 @@
     {#if !pokemonListOpened && !bagOpened}
         <div class="battleEnd" class:active={battleState && battleState?.ending || mainLoopContext.changingMap}></div>
     {/if}
+
+
     <div class="joysticks" bind:this={joysticks}></div>
 
     <!-- unused for now (keybooard/mouse or touch is enough -->
     <div class="ab-buttons" bind:this={abButtonsC}></div>
+
+    <div class="run-toggle">
+        {#if mainLoopContext.running}
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor"><path d="M9.82986 8.78986L7.99998 9.45588V13H5.99998V8.05H6.015L11.2834 6.13247C11.5274 6.03855 11.7922 5.99162 12.0648 6.0008C13.1762 6.02813 14.1522 6.75668 14.4917 7.82036C14.678 8.40431 14.848 8.79836 15.0015 9.0025C15.9138 10.2155 17.3653 11 19 11V13C16.8253 13 14.8823 12.0083 13.5984 10.4526L12.9008 14.4085L15 16.17V23H13V17.1025L10.7307 15.1984L10.003 19.3253L3.10938 18.1098L3.45667 16.1401L8.38071 17.0084L9.82986 8.78986ZM13.5 5.5C12.3954 5.5 11.5 4.60457 11.5 3.5C11.5 2.39543 12.3954 1.5 13.5 1.5C14.6046 1.5 15.5 2.39543 15.5 3.5C15.5 4.60457 14.6046 5.5 13.5 5.5Z"></path></svg>
+        {:else}
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor"><path d="M7.61713 8.71233L10.8222 6.38373C11.174 6.12735 11.6087 5.98543 12.065 6.0008C13.1764 6.02813 14.1524 6.75668 14.4919 7.82036C14.6782 8.40431 14.8481 8.79836 15.0017 9.0025C15.914 10.2155 17.3655 11 19.0002 11V13C16.8255 13 14.8825 12.0083 13.5986 10.4526L12.901 14.4085L14.9621 16.138L17.1853 22.246L15.3059 22.93L13.266 17.3256L9.87576 14.4808C9.32821 14.0382 9.03139 13.3192 9.16231 12.5767L9.67091 9.6923L8.99407 10.1841L6.86706 13.1116L5.24902 11.9361L7.60016 8.7L7.61713 8.71233ZM13.5002 5.5C12.3956 5.5 11.5002 4.60457 11.5002 3.5C11.5002 2.39543 12.3956 1.5 13.5002 1.5C14.6047 1.5 15.5002 2.39543 15.5002 3.5C15.5002 4.60457 14.6047 5.5 13.5002 5.5ZM10.5286 18.6813L7.31465 22.5116L5.78257 21.226L8.75774 17.6803L9.50426 15.5L11.2954 17L10.5286 18.6813Z"></path></svg>
+        {/if}
+
+        <label class="switch">
+            <input type="checkbox" on:change={mainLoopContext.running = !mainLoopContext.running}>
+            <span>
+            </span>
+        </label>
+    </div>
 </div>
 
 <script lang="ts">
@@ -107,6 +123,7 @@
      */
     function mainLoop() {
         mainLoopContext.id = window.requestAnimationFrame(mainLoop);
+        console.log(mainLoopContext.running)
 
         ctx.imageSmoothingEnabled = true;
         ctx.imageSmoothingQuality = 'high';
@@ -432,9 +449,6 @@
                 case 'ArrowLeft' :
                     keys.left.pressed = false;
                     break;
-                 case 'Shift':
-                     mainLoopContext.running = false;
-                     break;
             }
         }
     };
@@ -457,9 +471,6 @@
                     lastKey.key = 'ArrowLeft';
                     keys.left.pressed = true;
                     break;
-                 case 'Shift':
-                     mainLoopContext.running = true;
-                     break;
                 case 'x':
                     mainLoopContext.debug = !mainLoopContext.debug;
                     break;
@@ -518,12 +529,12 @@
         initContext();
 
         abButtons = new ABButtons(abButtonsC, (a, b) => {
-           /* keys.a.pressed = a;
-            keys.b.pressed = b;
-            console.log('a', a, 'b', b);*/
-            if(!mainLoopContext.playingScript){
+            /* keys.a.pressed = a;
+             keys.b.pressed = b;
+             console.log('a', a, 'b', b);*/
+            /*if (!mainLoopContext.playingScript) {
                 mainLoopContext.running = b;
-            }
+            }*/
         });
 
         joystick = new JoystickController({
@@ -571,18 +582,19 @@
   .start {
     font-family: pokemon, serif;
     position: absolute;
-    bottom: 5dvh;
+    bottom: 2dvh;
     left: calc(50% - 25px);
-    width: 60px;
+    height: 24px;
     box-sizing: border-box;
-    padding: 2px 0;
+    text-align: center;
+    padding: 0 16px;
     display: flex;
-    border-radius: 8px;
+    border-radius: 12px;
     background-color: rgba(44, 56, 69, 0.95);
     outline: none;
     z-index: 8;
     font-size: 18px;
-    border: 1px solid #000000;
+    border: none;
     color: white;
     justify-content: center;
     align-items: center;
@@ -648,5 +660,87 @@
     100% {
       opacity: 0;
     }
+  }
+
+  .run-toggle {
+
+    position: absolute;
+    bottom: 2dvh;
+    left: 1dvw;
+    z-index: 8;
+
+    display: flex;
+
+    svg {
+      height: 24px;
+      color: rgba(44, 56, 69, 0.95);
+    }
+
+    $primary: rgba(44, 56, 69, 0.95);
+    $lightGrey: rgba(44, 56, 69, 0.95);
+
+    .switch {
+      height: 24px;
+      display: block;
+      position: relative;
+      cursor: pointer;
+
+      input {
+        display: none;
+
+        & + span {
+          padding-left: 50px;
+          min-height: 24px;
+          line-height: 24px;
+          display: block;
+          color: $lightGrey;
+          position: relative;
+          vertical-align: middle;
+          white-space: nowrap;
+          transition: color .3s ease;
+
+          &:before,
+          &:after {
+            content: '';
+            display: block;
+            position: absolute;
+            border-radius: 12px;
+          }
+
+          &:before {
+            top: 0;
+            left: 0;
+            width: 42px;
+            height: 24px;
+            background: $lightGrey;
+            transition: all .3s ease;
+          }
+
+          &:after {
+            width: 18px;
+            height: 18px;
+            background: #fff;
+            top: 3px;
+            left: 3px;
+            box-shadow: 0 1px 3px rgba(#121621, .1);
+            transition: all .45s ease;
+          }
+        }
+
+        &:checked {
+          & + span {
+            &:before {
+              background: rgba($primary, .85);
+            }
+
+            &:after {
+              background: #fff;
+              transform: translate(18px, 0);
+            }
+          }
+        }
+      }
+    }
+
   }
 </style>
