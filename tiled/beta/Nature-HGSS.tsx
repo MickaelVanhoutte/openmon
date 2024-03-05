6507cfa222f3a43800818d6f40a05155c5b5a18f
Vanhoutte Mickael <mickael.vanhoutte@leroymerlin.fr>
2024-03-05 20:24:02 +0100

    chore: cleanup tiled project

--- tiled/beta/Nature-HGSS.tsx
+++ tiled/beta/Nature-HGSS.tsx
@@ -1,4 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset version="1.10" tiledversion="1.10.2" name="Nature-HGSS" tilewidth="16" tileheight="16" tilecount="5796" columns="92">
- <image source="../tileset/NatureRMXP.png" width="1472" height="1008"/>
+ <image source="../../tileset/NatureRMXP.png" width="1472" height="1008"/>
 </tileset>
--- tiled/beta/Urban-HGSS.tsx
+++ tiled/beta/Urban-HGSS.tsx
@@ -1,4 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset version="1.10" tiledversion="1.10.2" name="Urban-HGSS" tilewidth="16" tileheight="16" tilecount="4480" columns="56">
- <image source="../tileset/UrbanRMXP.png" width="896" height="1280"/>
+ <image source="../../tileset/UrbanRMXP.png" width="896" height="1280"/>
 </tileset>
