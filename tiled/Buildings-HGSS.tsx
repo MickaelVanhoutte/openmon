49d22ccf29ce41cf0101089bc236495bbcd7e5fb
Vanhoutte Mickael <mickael.vanhoutte@leroymerlin.fr>
2024-03-05 17:51:57 +0100

    feat: perfect tilesets !

---
+++ tiled/Buildings-HGSS.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.2" name="Buildings-HGSS" tilewidth="16" tileheight="16" tilecount="12960" columns="162">
+ <image source="../tileset/BuildingsRMXP.png" width="2592" height="1280"/>
+</tileset>
