7e65c3b78f77f3d082341cb8518e5acfc2ebb5dd
Mickael Vanhoutte <mickael.vanhoutte@leroymerlin.fr>
2024-03-09 15:46:57 +0100

    feat: wip save & contexts

--- src/js/common/contextV2.ts
+++ src/js/common/contextV2.ts
@@ -1,8 +1,13 @@
-import type {MapSave} from "../mapping/maps";
-import type {Player} from "../characters/player";
+import {MapSave} from "../mapping/maps";
+import {type Character, Player} from "../characters/player";
 import {OpenMap} from "../mapping/maps";
-import type {Settings} from "../characters/settings";
+import {Settings} from "../characters/settings";
 import {MAPS} from "../const";
+import {PokemonInstance} from "../pokemons/pokedex";
+import {BattleResult} from "../battle/battle";
+import {Position} from "../sprites/drawers";
+import {PokemonBox} from "../pokemons/boxes";
+import type {Script} from "./scripts";
 
 /**
  * Handles the save storage
@@ -10,17 +15,16 @@ import {MAPS} from "../const";
 export class SavesHolder {
 
         saves: SaveContext[] = [];
+        selectedSave?: SaveContext;
 
         constructor() {
             this.saves = localStorage.getItem('saves-v2')?.length &&
                 // @ts-ignore
-                JSON.parse(localStorage.getItem('saves-v2'))
+                JSON.parse(atob(localStorage.getItem('saves-v2')))
                     .map((save: string) => SaveContext.fromJSon(save))
                 || [];
-        }
 
-        addSave(save: SaveContext) {
-            this.saves.push(save);
+            this.saves = this.saves.sort((a, b) => b.updated - a.updated);
         }
 
         removeSave(index: number) {
@@ -28,11 +32,34 @@ export class SavesHolder {
         }
 
         getSave(index: number) {
+            this.selectedSave = this.saves[index];
             return this.saves[index];
         }
 
+        newGame(spriteId: number, name: string, gender: 'MALE' | 'FEMALE'): SaveContext {
+            let newSave = new SaveContext(
+                this.saves?.length || 0, Date.now(),
+                new MapSave( 0, {x: 0, y: 0}),
+                Player.fromScratch(spriteId, name, gender),
+                new Array<PokemonBox>(10).fill(new PokemonBox('Box', new Array<PokemonInstance | undefined>(36).fill(undefined))),
+                new Settings(),
+                true
+            );
+            this.saves.push(newSave);
+            this.persist();
+            return newSave;
+        }
+
         persist() {
-            localStorage.setItem('saves-v2', JSON.stringify(this.saves.map((save) => save.toJSon())));
+            if(this.selectedSave !== undefined &&
+                this.saves?.length > 0  &&
+                // @ts-ignore
+                this.saves.find((save) => save.id === this.selectedSave.id)) {
+                // @ts-ignore
+                this.saves.find((save) => save.id === this.selectedSave.id).updated = Date.now();
+            }
+            let encoded =  btoa(JSON.stringify(this.saves.map((save) => save.toJSon())));
+            localStorage.setItem('saves-v2', encoded);
         }
 
 }
@@ -42,20 +69,22 @@ export class SavesHolder {
  */
 export class SaveContext {
 
-    //id
-    //date
-    //player
-    //currentMap // position
-    //settings
-    //
+    id: number = 0;
+    updated: number = Date.now();
     currentMap: MapSave;
     player: Player;
+    boxes: Array<PokemonBox>;
     settings: Settings;
+    newGame: boolean;
 
-    constructor(currentMap: MapSave,  player: Player, settings: Settings) {
+    constructor(id: number, updated: number, currentMap: MapSave,  player: Player, boxes: Array<PokemonBox>, settings: Settings, newGame: boolean = false) {
+        this.id = id;
+        this.updated = updated;
         this.currentMap = currentMap;
         this.player = player;
+        this.boxes = boxes;
         this.settings = settings;
+        this.newGame = newGame;
     }
 
     toJSon() {
@@ -64,12 +93,13 @@ export class SaveContext {
 
     static fromJSon(json: string) {
         let fromJson = JSON.parse(json);
-        return new SaveContext(fromJson.currentMap, fromJson.player, fromJson.settings);
+        return new SaveContext(fromJson.id, fromJson.updated, fromJson.currentMap, fromJson.player, fromJson.boxes, fromJson.settings);
     }
 
     toGameContext(): GameContext {
         return new GameContext(
             this.player,
+            this.boxes,
             OpenMap.fromInstance(MAPS[this.currentMap.mapId], this.currentMap.playerPosition),
             this.settings
         );
@@ -82,35 +112,89 @@ export class SaveContext {
 export class GameContext {
 
     player: Player;
+    boxes: Array<PokemonBox>;
     map: OpenMap;
     settings: Settings;
 
     isPaused: boolean = false;
     isBattle: boolean = false;
 
-    constructor(player: Player, map: OpenMap, settings: Settings) {
+    battleContext?: BattleContext;
+
+    constructor(player: Player, boxes: Array<PokemonBox>, map: OpenMap, settings: Settings) {
         this.player = player;
+        this.boxes = boxes;
         this.map = map;
         this.settings = settings;
     }
 
     toOverworldContext() {
-        return new OverworldContext();
+        return new OverworldContext(this.player, this.map, this.settings);
     }
 
-    toBattleContext() {
-        return new BattleContext();
+    startBattle(opponent: PokemonInstance | Character): BattleContext {
+        let battleContext = new BattleContext((result) => {
+            this.isBattle = false;
+            this.battleContext = undefined;
+
+            if (!result.win) {
+                // tp back to the start // TODO pokecenter position
+                this.map.playerMovedOffset = new Position(0, 0);
+            } else if (result.caught) {
+                // add caught pokemon to team if space or in the box
+                if (this.player.monsters.length < 6) {
+                    this.player.monsters.push(result.caught);
+                } else {
+                    // first available space in boxes
+                    if(!this.boxes.every(box => box.isFull())){
+                        // @ts-ignore
+                        this.boxes[this.boxes.indexOf(this.boxes.find(box => !box.isFull()))].add(result.caught);
+                    }
+                }
+            }
+        });
+        this.isBattle = true;
+        this.battleContext = battleContext;
+        return battleContext;
+    }
+
+    togglePause() {
+        this.isPaused = !this.isPaused;
+    }
+
+    toSaveContext(): SaveContext {
+        return new SaveContext(0, Date.now(), new MapSave(this.map.mapId, this.map.playerMovedOffset), this.player, this.boxes, this.settings);
     }
 }
 
-/**
- *
- */
 export class OverworldContext {
 
+    player: Player;
+    map: OpenMap;
+    settings: Settings;
 
+    frames = {
+        frameId: 0,
+        then: Date.now(),
+        fpsInterval: 1000 / 18
+    }
+
+    changingMap: boolean = false;
+    playingScript?: Script;
+
+    constructor(player: Player, map: OpenMap, settings: Settings) {
+        this.player = player;
+        this.map = map;
+        this.settings = settings;
+    }
 }
 
 export class BattleContext {
+
+    onEnd: (result : BattleResult) => void = () => {};
+
+    constructor(onEnd: (result : BattleResult) => void) {
+        this.onEnd = onEnd;
+    }
 
 }
