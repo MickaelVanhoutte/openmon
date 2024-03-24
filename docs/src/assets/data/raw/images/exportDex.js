7aa945bcce0d827b860a3de0882816fe58e6b2c3
Perso <perso@MacBook-Pro-de-MICKAEL.local>
2024-03-24 10:37:03 +0100

    chore: publish

--- docs/src/assets/data/raw/images/exportDex.js
+++ docs/src/assets/data/raw/images/exportDex.js
@@ -1,13 +1,15 @@
 import fs from "fs";
 import ids from '../dex/pokemon-ids.json' assert {type: 'json'};
 
-ids.forEach((id) => {
+ids.forEach((id, index) => {
     let number = ("00" + id).slice(-3);
     let folder = '/Users/perso/workspace/perso/Pokemon/assets/images/'+ number;
     let normalFile = folder + '.png';
 
+    let newNumber = ("00" + (index + 1)).slice(-3);
+
     if(fs.existsSync(normalFile)) {
-        fs.copyFileSync(normalFile, '../../../monsters/pokedex/' + number + '.png');
+        fs.copyFileSync(normalFile, '../../../monsters/pokedex/' + newNumber + '.png');
     }else{
         console.log('Missing ' + number);
     }
--- docs/src/assets/data/raw/images/exportGifs.js
+++ docs/src/assets/data/raw/images/exportGifs.js
@@ -1,21 +1,23 @@
 import fs from "fs";
 import ids from '../dex/pokemon-ids.json' assert {type: 'json'};
 
-ids.forEach((id) => {
+ids.forEach((id, index) => {
     let number = ("00" + id).slice(-3);
-    let folder = '../monsters/bw-animated/';
+    let folder = '/Users/perso/workspace/perso/bw-animated/';
 
     let front = folder + number + '.gif';
     let frontS = folder + number + 's.gif';
     let back = folder + number + 'b.gif';
     let backS = folder + number + 'sb.gif';
 
+    let newNumber = ("00" + (index + 1)).slice(-3);
+
     if (fs.existsSync(front) && fs.existsSync(frontS) && fs.existsSync(back) && fs.existsSync(backS)) {
         //console.log('Copying ' + number + '...')
-        fs.copyFileSync(front, '../monsters/animated/' + number + '.gif');
-        fs.copyFileSync(frontS, '../monsters/animated/' + number + 's.gif');
-        fs.copyFileSync(back, '../monsters/animated/' + number + 'b.gif');
-        fs.copyFileSync(backS, '../monsters/animated/' + number + 'bs.gif');
+        fs.copyFileSync(front, '../../../monsters/animated/' + newNumber + '.gif');
+        fs.copyFileSync(frontS, '../../../monsters/animated/' + newNumber + 's.gif');
+        fs.copyFileSync(back, '../../..//monsters/animated/' + newNumber + 'b.gif');
+        fs.copyFileSync(backS, '../../../monsters/animated/' + newNumber + 'bs.gif');
     }else{
         console.log('Missing ' + number);
     }
