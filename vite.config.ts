bc98c3d6dd9c630d0663bf8a5cf81bbef9388ad1
Perso <perso@MacBook-Pro-de-MICKAEL.local>
2024-04-07 07:51:07 +0200

    fix: build

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,46 @@
 import { defineConfig } from 'vite'
 import { svelte } from '@sveltejs/vite-plugin-svelte'
+import sveltePreprocess from "svelte-preprocess";
+import {esbuildDecorators} from "@anatine/esbuild-decorators";
+import typescript from '@rollup/plugin-typescript';
+
+const noAttr = () => {
+  return {
+    name: "no-attribute",
+    transformIndexHtml(html: string) {
+      return html.replace(/crossorigin/g, "");
+    }
+}
+}
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [svelte()],
+  base: "./",
+  build: {
+    modulePreload: false,
+		rollupOptions: {
+			output: {
+				entryFileNames: "openmon.js",
+        assetFileNames: "openmon.[ext]",
+			},
+
+			input:[ "src/main.ts", "src/app.scss", "index.html"],
+		},
+	},
+  plugins: [
+    typescript(),
+		esbuildDecorators({
+				tsconfig: "./tsconfig.json",
+			}
+		),
+		svelte({
+			preprocess: sveltePreprocess(),
+			emitCss: false,
+			inspector: false,
+			compilerOptions: {
+				accessors: true,
+			},
+		}),
+    
+  ],
 })
