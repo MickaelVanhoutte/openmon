b603e4835897d0ad74a98b9109278f422460bc6f
Perso <perso@MacBook-Pro-de-MICKAEL.local>
2024-04-28 08:49:12 +0200

    feat: wip masteries

--- src/lib/menus/trainer/TrainerMastery.svelte
+++ src/lib/menus/trainer/TrainerMastery.svelte
@@ -1,7 +1,7 @@
 <script lang="ts">
 	import type { GameContext } from '../../../js/context/gameContext';
-	import { defineHex, Grid, rectangle, ring, spiral } from 'honeycomb-grid';
-	import { SVG } from '@svgdotjs/svg.js';
+	import { defineHex, Direction, Grid, rectangle, ring, spiral } from 'honeycomb-grid';
+	import { Svg, SVG } from '@svgdotjs/svg.js';
 	import { onMount } from 'svelte';
 	import initiateMasteries from '../../../assets/data/final/beta/masteries-initiate.json';
 	import expertMasteries from '../../../assets/data/final/beta/masteries-expert.json';
@@ -12,91 +12,143 @@
 	let expert;
 	let showModal = false;
 	let currentNode: any | undefined;
+	let currentTiles: any[];
 	$: masteryPoints = context.player.masteryPoints;
+
 	const initiateTiles = initiateMasteries;
+	let initiateSvg: Svg;
+	let initiateGrid: Grid;
 	const expertTiles = expertMasteries;
+	let expertSvg: Svg;
+	let expertGrid: Grid;
+
+	$: initiateFinished = initiateTiles.every((tile) => tile.set);
 
 	let count = 0;
 
-	function renderSVG(hex: HexExpert, draw: SVG.Doc, tiles: any[]) {
+	function hexToRGB(hex, alpha) {
+		var r = parseInt(hex.slice(1, 3), 16),
+			g = parseInt(hex.slice(3, 5), 16),
+			b = parseInt(hex.slice(5, 7), 16);
+
+		if (alpha) {
+			return 'rgba(' + r + ', ' + g + ', ' + b + ', ' + alpha + ')';
+		} else {
+			return 'rgb(' + r + ', ' + g + ', ' + b + ')';
+		}
+	}
+
+	function renderSVG(hex: HexExpert, draw: SVG.Doc, tiles: any[], grid: Grid) {
+		function isCurrentNeighborOfSetTile(hex: HexExpert, grid: Grid, tiles: any[]) {
+			return [
+				grid.neighborOf({ q: hex.q, r: hex.r }, Direction.NE, { allowOutside: false }),
+				grid.neighborOf({ q: hex.q, r: hex.r }, Direction.NW, { allowOutside: false }),
+				grid.neighborOf({ q: hex.q, r: hex.r }, Direction.E, { allowOutside: false }),
+				grid.neighborOf({ q: hex.q, r: hex.r }, Direction.W, { allowOutside: false }),
+				grid.neighborOf({ q: hex.q, r: hex.r }, Direction.SE, { allowOutside: false }),
+				grid.neighborOf({ q: hex.q, r: hex.r }, Direction.SW, { allowOutside: false })
+			]
+				.filter((neighbor) => !!neighbor)
+				.some((neighbor) => {
+					let neighborTile = tiles.find((t) => t.q === neighbor.q && t.r === neighbor.r);
+					if (neighborTile?.set) {
+						console.log('neighborTile: ', neighborTile);
+						return true;
+					}
+				});
+		}
+
 		let tile = tiles.find((tile) => tile.q === hex.q && tile.r === hex.r);
 
+		// get highest y value
+		let highestY = Math.min(
+			hex.corners[0].y,
+			hex.corners[1].y,
+			hex.corners[2].y,
+			hex.corners[3].y,
+			hex.corners[4].y,
+			hex.corners[5].y
+		);
+		let lowestY = Math.max(
+			hex.corners[0].y,
+			hex.corners[1].y,
+			hex.corners[2].y,
+			hex.corners[3].y,
+			hex.corners[4].y,
+			hex.corners[5].y
+		);
+
+		let highestX = Math.min(
+			hex.corners[0].x,
+			hex.corners[1].x,
+			hex.corners[2].x,
+			hex.corners[3].x,
+			hex.corners[4].x,
+			hex.corners[5].x
+		);
+		let lowestX = Math.max(
+			hex.corners[0].x,
+			hex.corners[1].x,
+			hex.corners[2].x,
+			hex.corners[3].x,
+			hex.corners[4].x,
+			hex.corners[5].x
+		);
+
+		let width = Math.abs(lowestX - highestX);
+		let height = Math.abs(lowestY - highestY);
+
 		if (tile) {
+			let hasSetNeighbor = isCurrentNeighborOfSetTile(hex, grid, tiles);
 			const polygon = draw
 				// create a polygon from a hex's corner points
 				.polygon(hex.corners.map(({ x, y }) => `${x},${y}`))
 				.fill('white')
 				.css('cursor', 'pointer')
 				.css('text-align', 'center')
-				.stroke({ width: 2, color: tile?.color || '#999' });
+				.stroke({ width: 2, color: 'white' });
 
 			count += tile.cost;
-			if (tile?.color) {
-				polygon.fill(tile.color);
-				if (tile.set) {
-				} else {
-					//polygon.fill('#EEE');
-					polygon.css('opacity', '0.7');
-					polygon.stroke({ width: 2, color: tile.color });
-				}
-			} else {
-				polygon.fill('white');
+
+			polygon.fill(tile.color);
+			if (tile.first) {
+				draw
+					.text('First')
+					.move(hex.corners[0].x - width / 2, hex.corners[0].y - 14)
+					.css('pointer-events', 'none')
+					.css('text-anchor', 'middle')
+					.font({
+						family: 'pokemon',
+						fill: 'black',
+						size: 20,
+						weight: 'bold',
+						leading: 1
+					});
+			} else if (!tile.set && !hasSetNeighbor && !tile.first) {
+				polygon.fill(hexToRGB(tile.color, 0.3));
+			} else if (!tile.set && hasSetNeighbor && !tile.first) {
+				polygon.fill(hexToRGB(tile.color, 0.6));
+				tile.settable = true;
 			}
 
-			
-
-			// get highest y value 
-			let highestY = Math.min(hex.corners[0].y, hex.corners[1].y, hex.corners[2].y, hex.corners[3].y, hex.corners[4].y, hex.corners[5].y);
-			let lowestY = Math.max(hex.corners[0].y, hex.corners[1].y, hex.corners[2].y, hex.corners[3].y, hex.corners[4].y, hex.corners[5].y);
-
-			let highestX = Math.min(hex.corners[0].x, hex.corners[1].x, hex.corners[2].x, hex.corners[3].x, hex.corners[4].x, hex.corners[5].x);
-			let lowestX = Math.max(hex.corners[0].x, hex.corners[1].x, hex.corners[2].x, hex.corners[3].x, hex.corners[4].x, hex.corners[5].x);
-
-			let width = Math.abs(lowestX - highestX);
-			let height = Math.abs(lowestY - highestY);
-			
-
 			draw.group().add(polygon);
-			//console.log(polygon, hex.corners);
+
 			if (tile?.title) {
-				if (tile.title?.split(' ')?.length > 1) {
-					tile.title.split(' ').forEach((word, i) => {
-						draw
-							.text(word)
-							// .move(
-							// 	hex.corners[0].x - width / 2,
-							// 	highestY  + ((i) * (height / 4)) //- (hex.corners[2].y - hex.corners[1].y) + (i * width) / 4
-							// )
-							.move(hex.corners[0].x - width / 2, hex.corners[0].y - 14 + i * (height/4))
-							.css('pointer-events', 'none')
-							.css('text-anchor', 'middle')
-							.css('transform', 'translate(100)')
-							.font({
-								family: 'pokemon',
-								fill: '#444',
-								size: height / 4,
-								weight: 'bold',
-								leading: 1
-							});
-					});
-				} else {
+				tile.title.split(' ').forEach((word, i) => {
 					draw
-						.text(tile.title)
-						// .move(
-						// 	hex.corners[0].x - width / 2,
-						// 	highestY  //+ height / 2, //- (hex.corners[2].y - hex.corners[1].y)
-						// )
-						.move(hex.corners[0].x - width / 2, hex.corners[0].y - 14 )
+						.text(word)
+						.move(hex.corners[0].x - width / 2, hex.corners[0].y - 14 + i * (height / 4))
 						.css('pointer-events', 'none')
+						.css('text-anchor', 'middle')
+						.css('transform', 'translate(100)')
 						.font({
 							family: 'pokemon',
-							fill: '#444',
-							size: height / 4,
-							anchor: 'middle',
+							fill: !tile.set && hasSetNeighbor ? 'gold' : tile.set ? '#444' : '#EEE',
+							size: width / 4,
 							weight: 'bold',
 							leading: 1
 						});
-				}
+				});
 			}
 		} else {
 			const polygon = draw;
@@ -108,51 +160,80 @@
 		return;
 	}
 
-	function openModal(tile?: any): any {
+	function openModal(tile?: any, tiles: any[]): any {
 		if (tile && !tile.first) {
 			currentNode = tile;
+			currentTiles = tiles;
 			showModal = true;
 		}
 	}
 
+	function drawGrid(draw: Svg, grid: Grid, tiles: any[]) {
+		grid.forEach((hex) => renderSVG(hex, draw, tiles, grid));
+	}
+
+	function setNode(tile: any, tiles: any[]) {
+		tile.set = true;
+		tile.settable = false;
+		masteryPoints -= tile.cost;
+		tiles === initiateTiles
+			? drawGrid(initiateSvg, initiateGrid, initiateTiles)
+			: drawGrid(expertSvg, expertGrid, expertTiles);
+		showModal = false;
+	}
+
 	onMount(() => {
 		// INITIATE
-		const draw = SVG().addTo(masteries).size('100%', '100%');
+		initiateSvg = SVG().addTo(masteries).size('100%', '100%');
 		const Hex = defineHex({
 			dimensions: masteries.getBoundingClientRect().width / 28.4,
 			origin: 'topLeft'
 		});
-		const grid = new Grid(Hex, rectangle({ width: 16, height: 1 }));
-		console.log(grid.toArray()[0].corners);
-		grid.forEach((hex) => renderSVG(hex, draw, initiateTiles));
+		initiateGrid = new Grid(Hex, rectangle({ width: 16, height: 1 }));
+
+		drawGrid(initiateSvg, initiateGrid, initiateTiles);
+
 		masteries.addEventListener('click', ({ offsetX, offsetY }) => {
-			const hex = grid.pointToHex({ x: offsetX, y: offsetY }, { allowOutside: false });
-			openModal(initiateTiles.find((tile) => tile.q === hex.q && tile.r === hex.r));
+			const hex = initiateGrid.pointToHex({ x: offsetX, y: offsetY }, { allowOutside: false });
+			openModal(
+				initiateTiles.find((tile) => tile.q === hex.q && tile.r === hex.r),
+				initiateTiles
+			);
 		});
 		masteries.addEventListener('touch', ({ offsetX, offsetY }) => {
-			const hex = grid.pointToHex({ x: offsetX, y: offsetY }, { allowOutside: false });
-			openModal(initiateTiles.find((tile) => tile.q === hex.q && tile.r === hex.r));
+			const hex = initiateGrid.pointToHex({ x: offsetX, y: offsetY }, { allowOutside: false });
+			openModal(
+				initiateTiles.find((tile) => tile.q === hex.q && tile.r === hex.r),
+				initiateTiles
+			);
 		});
 
 		// EXPERT
-		const draw2 = SVG().addTo(expert).size('100%', '100%');
+		expertSvg = SVG().addTo(expert).size('100%', '100%');
 		const Hex2 = defineHex({
 			dimensions: expert.getBoundingClientRect().width / 26.5,
 			origin: 'topLeft'
 		});
-		const grid2 = new Grid(Hex2, rectangle({ width: 15, height: 4 }));
-		grid2.forEach((hex) => renderSVG(hex, draw2, expertTiles));
-		expert.addEventListener('click', ({ offsetX, offsetY }) => {
-			const hex = grid2.pointToHex({ x: offsetX, y: offsetY }, { allowOutside: false });
-			openModal(expertTiles.find((tile) => tile.q === hex.q && tile.r === hex.r));
+		expertGrid = new Grid(Hex2, rectangle({ width: 15, height: 4 }));
+		drawGrid(expertSvg, expertGrid, expertTiles);
+		-expert.addEventListener('click', ({ offsetX, offsetY }) => {
+			const hex = expertGrid.pointToHex({ x: offsetX, y: offsetY }, { allowOutside: false });
+			openModal(
+				expertTiles.find((tile) => tile.q === hex.q && tile.r === hex.r),
+				expertTiles
+			);
 		});
 		expert.addEventListener('touch', ({ offsetX, offsetY }) => {
-			const hex = grid2.pointToHex({ x: offsetX, y: offsetY }, { allowOutside: false });
-			openModal(expertTiles.find((tile) => tile.q === hex.q && tile.r === hex.r));
+			const hex = expertGrid.pointToHex({ x: offsetX, y: offsetY }, { allowOutside: false });
+			openModal(
+				expertTiles.find((tile) => tile.q === hex.q && tile.r === hex.r),
+				expertTiles
+			);
 		});
 		console.log(count);
 	});
 </script>
+
 <div class="offset"></div>
 <div class="masteries" bind:this={masteries}></div>
 <div class="expert" bind:this={expert}></div>
@@ -174,10 +255,18 @@
 	<p style="margin: 0">
 		{currentNode?.description}
 	</p>
+	<hr />
+	<button
+		class="button primary"
+		disabled={!currentNode?.settable}
+		on:click={() => {
+			setNode(currentNode, currentTiles);
+		}}>Activate</button
+	>
 </Modal>
 
 <style lang="scss">
-	.offset{
+	.offset {
 		//height: 2dvh;
 		width: 100%;
 	}
