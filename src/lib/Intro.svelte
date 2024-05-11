0373a488d65072ea75f44c0593434cfa2ca8e378
Perso <perso@MacBook-Pro-de-MICKAEL.local>
2024-05-11 10:11:17 +0200

    fix: intro

--- src/lib/Intro.svelte
+++ src/lib/Intro.svelte
@@ -273,18 +273,12 @@
 
 			background: #0f0c29; /* fallback for old browsers */
 			background: -webkit-linear-gradient(
-				to right,
-				#24243e,
-				#302b63,
-				#0f0c29
+				to right, #24243e, #5b53b2, #0f0c29
 			); /* Chrome 10-25, Safari 5.1-6 */
 			background: linear-gradient(
-				to right,
-				#24243e,
-				#302b63,
-				#0f0c29
+				to right, #24243e, #5b53b2, #0f0c29
 			); /* W3C, IE 10+/ Edge, Firefox 16+, Chrome 26+, Opera 12+, Safari 7+ */
-
+      
 			position: fixed;
 			//z-index: 9999;
 			left: 0;
