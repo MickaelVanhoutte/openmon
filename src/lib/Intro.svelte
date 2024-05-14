8c764c67c51aaab2e0359dce3c2612426d6ffa3d
Perso <perso@MacBook-Pro-de-MICKAEL.local>
2024-05-14 22:13:28 +0200

    feat: wip sound

--- src/lib/Intro.svelte
+++ src/lib/Intro.svelte
@@ -1,9 +1,13 @@
 <script lang="ts">
 	import { onMount } from 'svelte';
-
+	import { Howl } from 'howler';
+  import { fade } from 'svelte/transition';
 
 	export let started: boolean;
 	let intro: HTMLDivElement;
+	let horde: HTMLDivElement;
+	let sound: Howl;
+	let soundPlaying = false;
 	let pkmnListShuffled: number[] = fisherYates(Array.from({ length: 233 }, (_, i) => i)).slice(
 		0,
 		35
@@ -24,28 +28,64 @@
 		return array;
 	}
 
+	function toggleSound() {
+		if (!soundPlaying) {
+			sound.mute(false);
+			soundPlaying = true;
+		} else {
+			sound.mute(true);
+			soundPlaying = false;
+		}
+	}
+
+	function loadSound() {
+		sound = new Howl({
+			src: ['src/assets/audio/intro.mp3'],
+			autoplay: true,
+			loop: true,
+			volume: 0.5
+		});
+		setTimeout(() => {
+			soundPlaying = sound.playing();
+		}, 200);
+		console.log(sound);
+	}
+
 	onMount(() => {
-		let audio = new Audio('src/assets/audio/intro.mp3');
-    audio.volume = 0.5;
-    audio.play();
-    console.log(audio);
+		loadSound();
 		setTimeout(() => {
 			ready = true;
 		}, 10000);
-		intro.addEventListener('click', () => {
+		horde.addEventListener('click', () => {
 			if (ready) started = true;
 		});
 
-    return () => {
-      audio.pause();
-    }
+		return () => {
+			sound.stop();
+		};
 	});
 </script>
 
-<div class="intro" bind:this={intro}>
+<div class="intro" bind:this={intro} out:fade>
 	{#each Array.from({ length: 8 }) as i}
 		<div class="firefly"></div>
 	{/each}
+
+	<span class="sound" on:click={toggleSound}>
+		{#if soundPlaying}
+			<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor"
+				><path
+					d="M5.88889 16H2C1.44772 16 1 15.5523 1 15V9.00001C1 8.44772 1.44772 8.00001 2 8.00001H5.88889L11.1834 3.66815C11.3971 3.49329 11.7121 3.52479 11.887 3.73851C11.9601 3.82784 12 3.93971 12 4.05513V19.9449C12 20.221 11.7761 20.4449 11.5 20.4449C11.3846 20.4449 11.2727 20.405 11.1834 20.3319L5.88889 16ZM20.4142 12L23.9497 15.5355L22.5355 16.9498L19 13.4142L15.4645 16.9498L14.0503 15.5355L17.5858 12L14.0503 8.46447L15.4645 7.05026L19 10.5858L22.5355 7.05026L23.9497 8.46447L20.4142 12Z"
+				></path></svg
+			>
+		{:else}
+			<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor"
+				><path
+					d="M2 16.0001H5.88889L11.1834 20.3319C11.2727 20.405 11.3846 20.4449 11.5 20.4449C11.7761 20.4449 12 20.2211 12 19.9449V4.05519C12 3.93977 11.9601 3.8279 11.887 3.73857C11.7121 3.52485 11.3971 3.49335 11.1834 3.66821L5.88889 8.00007H2C1.44772 8.00007 1 8.44778 1 9.00007V15.0001C1 15.5524 1.44772 16.0001 2 16.0001ZM23 12C23 15.292 21.5539 18.2463 19.2622 20.2622L17.8445 18.8444C19.7758 17.1937 21 14.7398 21 12C21 9.26016 19.7758 6.80629 17.8445 5.15557L19.2622 3.73779C21.5539 5.75368 23 8.70795 23 12ZM18 12C18 10.0883 17.106 8.38548 15.7133 7.28673L14.2842 8.71584C15.3213 9.43855 16 10.64 16 12C16 13.36 15.3213 14.5614 14.2842 15.2841L15.7133 16.7132C17.106 15.6145 18 13.9116 18 12Z"
+				></path></svg
+			>
+		{/if}
+	</span>
 
 	<img class="logo" src="src/assets/menus/pokemon-logo.png" alt="pokemon logo" />
 
@@ -74,7 +114,7 @@
 	<img class="diancie" src="src/assets/diancie.png" alt="diancie" />
 	<span class="touch">Touch to start</span>
 
-	<div class="horde">
+	<div class="horde" bind:this={horde}>
 		{#each pkmnListShuffled as i, index}
 			<i
 				class="sprite pokemon move"
@@ -205,18 +245,22 @@
 	@keyframes shadow1 {
 		0% {
 			filter: drop-shadow(0 0 2px #fff) drop-shadow(0 0 3px #4444dd) drop-shadow(0 0 5px #4444dd);
+			bottom: 25%;
 		}
 		100% {
 			filter: drop-shadow(0 0 5px #fff) drop-shadow(0 0 6px #4444dd) drop-shadow(0 0 10px #4444dd);
+			bottom: 22%;
 		}
 	}
 
 	@keyframes shadow2 {
 		0% {
 			filter: drop-shadow(0 0 4px #000) drop-shadow(0 0 3px #7f7fef) drop-shadow(0 0 5px #a8a8ec);
+			bottom: 22%;
 		}
 		100% {
 			filter: drop-shadow(0 0 7px #000) drop-shadow(0 0 6px #7f7fef) drop-shadow(0 0 10px #a8a8ec);
+			bottom: 25%;
 		}
 	}
 
@@ -233,6 +277,22 @@
 		z-index: 100;
 		overflow: hidden;
 		perspective: 100dvw;
+
+		.sound {
+			position: absolute;
+			top: 3%;
+			left: 3%;
+			z-index: 110;
+			width: 42px;
+			height: 42px;
+			color: white;
+			opacity: 0.7;
+
+			svg {
+				width: 32px;
+				height: 32px;
+			}
+		}
 
 		.links {
 			position: absolute;
@@ -260,14 +320,14 @@
 		}
 
 		.darkrai {
-			will-change: transform, opacity, filter;
+			will-change: transform, opacity, filter, bottom;
 			position: absolute;
 			bottom: 22%;
 			left: 3%;
 			width: 100%;
 			max-width: 24dvw;
 			opacity: 0;
-			transform: translateX(-100%) rotate(15deg) scale(0.55) rotateY(0);
+			transform: translateX(-100%) rotate(15deg) scale(0.55) rotateY(0) translateY(0);
 			animation:
 				fromLeft 6s ease-in-out forwards,
 				shadow1 3s ease-in-out infinite alternate;
@@ -275,18 +335,18 @@
 			z-index: 96;
 		}
 		.diancie {
-			will-change: transform, opacity, filter;
+			will-change: transform, opacity, filter, bottom;
 			position: absolute;
 			bottom: 22%;
 			right: 3%;
 			width: 100%;
 			max-width: 24dvw;
 			opacity: 0;
-			transform: translateX(100%) rotate(-15deg) scale(0.55) rotateY(0);
+			transform: translateX(100%) rotate(-15deg) scale(0.55) rotateY(0) translateY(0);
 			animation:
 				fromRight 6s ease-in-out forwards,
 				shadow2 3s ease-in-out infinite alternate;
-			animation-delay: 6s;
+			animation-delay: 6s, 6.4s;
 			z-index: 96;
 		}
 
