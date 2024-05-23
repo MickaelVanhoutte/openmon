554c3347144360c6f7ba2ee1f639dd50a52bbaae
Perso <perso@MacBook-Pro-de-MICKAEL.local>
2024-05-23 21:39:21 +0200

    feat: shop ui

--- src/lib/common/Shop.svelte
+++ src/lib/common/Shop.svelte
@@ -32,12 +32,10 @@
 			selected = Math.max(selected - 1, 0);
 		}
 		if (e.key === 'ArrowUp' && openQty) {
-			if ((qty + 1) * selectedItem.price <= context.player.bag.money) {
-				qty = qty + 1 > 99 ? 0 : qty + 1;
-			}
+			more();
 		}
 		if (e.key === 'ArrowDown' && openQty) {
-			qty = qty - 1 < 1 ? Math.floor(context.player.bag.money / selectedItem.price) : qty - 1;
+			less();
 		}
 		if (e.key === 'Enter') {
 			if (!openQty) {
@@ -47,6 +45,16 @@
 			}
 		}
 	};
+
+	function more() {
+		if ((qty + 1) * selectedItem.price <= context.player.bag.money) {
+			qty = qty + 1 > 99 ? 0 : qty + 1;
+		}
+	}
+
+	function less() {
+		qty = qty - 1 < 1 ? Math.floor(context.player.bag.money / selectedItem.price) : qty - 1;
+	}
 
 	function cancel() {
 		openQty = false;
@@ -85,7 +93,6 @@
 <div class="shop">
 	<div class="desc">
 		<div class="inner">
-			<span>{items[selected].name}</span>
 			<span>{items[selected].desc}</span>
 		</div>
 	</div>
@@ -96,7 +103,13 @@
 		</div>
 		<ul class="items">
 			{#each items as item, index}
-				<li class:selected={selected === index}>
+				<li
+					class:selected={selected === index}
+					on:click={() => {
+						selected = index;
+						openQty = true;
+					}}
+				>
 					<span>{item.name}</span>
 					<span>{item.price} $</span>
 				</li>
@@ -105,16 +118,32 @@
 
 		{#if openQty}
 			<div class="qty">
-				<span>
-					x{qty} = {qty * selectedItem.price}$
-				</span>
+				<div class="value">
+					<span>
+						x{qty} = {qty * selectedItem.price}$
+					</span>
+					<div class="more-less">
+						<button on:click={() => more()}>+</button>
+						<button on:click={() => less()}>-</button>
+					</div>
+				</div>
 
-				<div>
-					<button class="button error" on:click={() => cancel()}>Cancel</button>
+				<div class="buy-cancel">
+					<button class="button outline" on:click={() => cancel()}>Cancel</button>
 					<button class="button primary" on:click={() => buy()}>Buy</button>
 				</div>
 			</div>
 		{/if}
+	</div>
+
+	<div class="close">
+		<button on:click={() => context.playingScript?.nextAction(context)}>
+			<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor"
+				><path
+					d="M10.5859 12L2.79297 4.20706L4.20718 2.79285L12.0001 10.5857L19.793 2.79285L21.2072 4.20706L13.4143 12L21.2072 19.7928L19.793 21.2071L12.0001 13.4142L4.20718 21.2071L2.79297 19.7928L10.5859 12Z"
+				></path></svg
+			>
+		</button>
 	</div>
 </div>
 
@@ -128,6 +157,22 @@
 		background-color: rgba(0, 0, 0, 0.5);
 		z-index: 100;
 		color: white;
+
+		.close {
+			position: absolute;
+			top: 4%;
+			left: 2%;
+			width: 60px;
+
+			button {
+				height: 60px;
+				width: 60px;
+				color: white;
+				background-color: transparent;
+				border: none;
+				outline: none;
+			}
+		}
 
 		.desc {
 			position: absolute;
@@ -160,10 +205,10 @@
 				border-radius: 9px;
 				background-color: rgba(0, 0, 0, 0.5);
 				position: absolute;
-				bottom: 16px;
-				left: 16px;
-				width: calc(50dvw - 32px);
-				height: calc(25dvh - 32px);
+				bottom: 8px;
+				left: 8px;
+				width: calc(50dvw - 16px);
+				height: calc(25dvh - 16px);
 				display: flex;
 				flex-direction: column;
 				justify-content: space-around;
@@ -227,27 +272,54 @@
 				bottom: 16px;
 				right: 16px;
 				width: calc(50dvw - 32px);
-				height: calc(25dvh - 32px);
+				padding: 8px;
 				background-color: rgba(0, 0, 0, 0.5);
-				z-index: 101;
+                border-radius: 8px;
+				z-index: 102;
 				display: flex;
-				justify-content: space-around;
+				gap: 8px;
+				flex-wrap: wrap;
+				justify-content: space-between;
 				align-items: center;
 
-				span {
-					// width: 50%;
-					// height: 50%;
-					padding: 16px;
-					font-size: 32 px;
-					text-align: center;
-					background-color: rgba(0, 0, 0, 0.5);
-					color: white;
-					border: none;
-					border-radius: 9px;
+				.value {
+					display: flex;
+					gap: 8px;
+					align-items: center;
+
+					span {
+						padding: 16px;
+						font-size: 32 px;
+						text-align: center;
+						background-color: rgba(0, 0, 0, 0.5);
+						color: white;
+						border: none;
+						border-radius: 9px;
+					}
+
+					.more-less {
+						display: flex;
+						flex-direction: column;
+						gap: 8px;
+
+						button {
+							height: 28px;
+							width: 42px;
+
+							border-radius: 4px;
+							border: 1px solid black;
+							background: rgba(84, 80, 108, 0.2);
+							color: white;
+                            font-size: 28px;
+						}
+					}
 				}
 
-				button {
-					font-size: 32px;
+				.buy-cancel {
+					button {
+						font-size: 32px;
+                        color: white;
+					}
 				}
 			}
 		}
