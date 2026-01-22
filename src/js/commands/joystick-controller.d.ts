d65c70fe760bf2a005630cd64807addf1c4ff41a
MickaelVanhoutte <mickael.vanhoutte@adeo.com>
2026-01-22 21:40:47 +0100

    feat: wip refactor

--- src/js/commands/joystick-controller.d.ts
+++ src/js/commands/joystick-controller.d.ts
@@ -1,1 +1,50 @@
-// declare module 'joystick-controller';
+export interface JoystickOptions {
+	maxRange?: number;
+	level?: number;
+	radius?: number;
+	joystickRadius?: number;
+	opacity?: number;
+	containerClass?: string;
+	controllerClass?: string;
+	joystickClass?: string;
+	leftToRight?: boolean;
+	bottomToUp?: boolean;
+	x?: string;
+	y?: string;
+	distortion?: boolean;
+	dynamicPosition?: boolean;
+	dynamicPositionTarget?: HTMLElement | null;
+	mouseClickButton?: string | number;
+	hideContextMenu?: boolean;
+}
+
+export interface JoystickOnMove {
+	x: number;
+	y: number;
+	leveledX: number;
+	leveledY: number;
+	angle: number;
+	distance: number;
+}
+
+export const MOUSE_CLICK_BUTTONS: {
+	ALL: -1;
+	LEFT: 0;
+	MIDDLE: 1;
+	RIGHT: 2;
+};
+
+export class JoystickController {
+	id: string;
+	x: number;
+	y: number;
+	leveledX: number;
+	leveledY: number;
+	angle: number;
+	distance: number;
+
+	constructor(options: JoystickOptions, onMove?: (coordinates: JoystickOnMove) => void);
+	destroy(): void;
+}
+
+export default JoystickController;
---
+++ src/js/pokemons/effects/types.ts
@@ -0,0 +1,57 @@
+import '@abraham/reflection';
+import { PokemonInstance } from '../pokedex';
+
+export enum EffectTiming {
+	START_TURN = 'start-turn',
+	END_TURN = 'end-turn',
+	BEFORE_MOVE = 'before-move',
+	AFTER_MOVE = 'after-move',
+	BEFORE_SWITCH = 'before-switch',
+	AFTER_SWITCH = 'after-switch',
+	BEFORE_FAINT = 'before-faint',
+	AFTER_FAINT = 'after-faint'
+}
+
+export class EffectResult {
+	message?: string;
+	effect?: Effect;
+
+	constructor(effect?: Effect, message?: string) {
+		this.message = message;
+		this.effect = effect;
+	}
+}
+
+export class EffectForTurn {
+	message?: string;
+	canPlay?: boolean = true;
+
+	constructor(canPlay?: boolean, message?: string) {
+		this.canPlay = canPlay;
+		this.message = message;
+	}
+}
+
+export interface Effect {
+	move_effect_id: number;
+	abr: string;
+	duration: number;
+	when: EffectTiming;
+	damages: number;
+
+	turnsPassed: number;
+	healed: boolean;
+
+	apply(target: PokemonInstance[], user?: PokemonInstance): EffectResult;
+
+	playEffect(target: PokemonInstance, user?: PokemonInstance): EffectForTurn;
+}
+
+export const DEFAULT_EFFECT_PROPS = {
+	abr: '',
+	duration: -1,
+	when: EffectTiming.AFTER_MOVE,
+	damages: 0,
+	turnsPassed: 0,
+	healed: false
+};
